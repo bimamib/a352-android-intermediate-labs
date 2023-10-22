3d708755afd9e2ee137dc62c5d4b31c29afb3ab9
bimamib <bimaprast1601@gmail.com>
2023-10-22 10:40:52 +0700

    menyiapkan Fake datasource yang sudah ada

--- advanced-testing/LatihanAdcancedTesting (coroutines)/app/src/test/java/com/dicoding/newsapp/data/NewsRepositoryTest.kt
+++ advanced-testing/LatihanAdcancedTesting (coroutines)/app/src/test/java/com/dicoding/newsapp/data/NewsRepositoryTest.kt
@@ -1,5 +1,29 @@
 package com.dicoding.newsapp.data
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.dicoding.newsapp.MainDispatcherRule
+import com.dicoding.newsapp.data.local.room.NewsDao
+import com.dicoding.newsapp.data.remote.retrofit.ApiService
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 
-class NewsRepositoryTest
+@ExperimentalCoroutinesApi
+class NewsRepositoryTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var apiService: ApiService
+    private lateinit var newsDao : NewsDao
+    private lateinit var newsRepository: NewsRepository
+
+    @Before
+    fun setUp() {
+        apiService = FakeApiService()
+        newsDao = FakeNewsDao()
+        newsRepository = NewsRepository(apiService, newsDao)
+    }
+}
