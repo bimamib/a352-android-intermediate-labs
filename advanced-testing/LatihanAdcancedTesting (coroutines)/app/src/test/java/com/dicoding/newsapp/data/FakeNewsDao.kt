bf4799e565d2da101406013863a8ce39a976c157
bimamib <bimaprast1601@gmail.com>
2023-10-22 10:26:22 +0700

    implements member

--- advanced-testing/LatihanAdcancedTesting (coroutines)/app/src/test/java/com/dicoding/newsapp/data/FakeNewsDao.kt
+++ advanced-testing/LatihanAdcancedTesting (coroutines)/app/src/test/java/com/dicoding/newsapp/data/FakeNewsDao.kt
@@ -1,4 +1,23 @@
 package com.dicoding.newsapp.data
 
-class FakeNewsDao {
+import androidx.lifecycle.LiveData
+import com.dicoding.newsapp.data.local.entity.NewsEntity
+import com.dicoding.newsapp.data.local.room.NewsDao
+
+class FakeNewsDao : NewsDao {
+    override fun getBookmarkedNews(): LiveData<List<NewsEntity>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveNews(news: NewsEntity) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNews(newsTitle: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isNewsBookmarked(title: String): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
 }
