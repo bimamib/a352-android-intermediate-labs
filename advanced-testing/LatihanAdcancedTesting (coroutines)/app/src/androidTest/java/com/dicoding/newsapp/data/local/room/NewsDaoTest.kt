d982b0f9e08bcbab4573459fde336751d53bc869
bimamib <bimaprast1601@gmail.com>
2023-10-22 17:21:58 +0700

    update

--- advanced-testing/LatihanAdcancedTesting (coroutines)/app/src/androidTest/java/com/dicoding/newsapp/data/local/room/NewsDaoTest.kt
+++ advanced-testing/LatihanAdcancedTesting (coroutines)/app/src/androidTest/java/com/dicoding/newsapp/data/local/room/NewsDaoTest.kt
@@ -3,10 +3,14 @@ package com.dicoding.newsapp.data.local.room
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import com.dicoding.newsapp.utils.DataDummy
+import com.dicoding.newsapp.utils.getOrAwaitValue
+import kotlinx.coroutines.test.runTest
 import org.junit.After
-import org.junit.Assert.*
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class NewsDaoTest {
 
@@ -15,6 +19,7 @@ class NewsDaoTest {
 
     private lateinit var database: NewsDatabase
     private lateinit var dao: NewsDao
+    private val sampleNews = DataDummy.generateDummyNewsEntity()[0]
 
     @Before
     fun initDb() {
@@ -27,4 +32,21 @@ class NewsDaoTest {
 
     @After
     fun closeDb() = database.close()
+
+    @Test
+    fun saveNews_Success() = runTest {
+        dao.saveNews(sampleNews)
+        val actualNews = dao.getBookmarkedNews().getOrAwaitValue()
+        Assert.assertEquals(sampleNews.title, actualNews[0].title)
+        Assert.assertTrue(dao.isNewsBookmarked(sampleNews.title).getOrAwaitValue())
+    }
+
+    @Test
+    fun deleteNews_Success() = runTest {
+        dao.saveNews(sampleNews)
+        dao.deleteNews(sampleNews.title)
+        val actualNews = dao.getBookmarkedNews().getOrAwaitValue()
+        Assert.assertTrue(actualNews.isEmpty())
+        Assert.assertFalse(dao.isNewsBookmarked(sampleNews.title).getOrAwaitValue())
+    }
 }
