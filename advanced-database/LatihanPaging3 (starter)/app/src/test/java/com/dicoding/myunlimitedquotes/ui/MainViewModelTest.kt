f36211c21da72dd20d48bc6a4efb48db7fc2a653
bimamib <bimaprast1601@gmail.com>
2023-10-23 23:05:00 +0700

    memasukkan PaggingData ke dalam adapter

--- advanced-database/LatihanPaging3 (starter)/app/src/test/java/com/dicoding/myunlimitedquotes/ui/MainViewModelTest.kt
+++ advanced-database/LatihanPaging3 (starter)/app/src/test/java/com/dicoding/myunlimitedquotes/ui/MainViewModelTest.kt
@@ -3,14 +3,18 @@ package com.dicoding.myunlimitedquotes.ui
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.AsyncPagingDataDiffer
 import androidx.paging.PagingData
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import androidx.recyclerview.widget.ListUpdateCallback
 import com.dicoding.myunlimitedquotes.DataDummy
 import com.dicoding.myunlimitedquotes.MainDispatcherRule
+import com.dicoding.myunlimitedquotes.adapter.QuoteListAdapter
 import com.dicoding.myunlimitedquotes.data.QuoteRepository
 import com.dicoding.myunlimitedquotes.getOrAwaitValue
 import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
@@ -39,12 +43,26 @@ class MainViewModelTest {
         val data: PagingData<QuoteResponseItem> = QuotePagingSource.snapshot(dummyQuote)
         val expectedQuote = MutableLiveData<PagingData<QuoteResponseItem>>()
         expectedQuote.value = data
-        Mockito.`when`(quoteRepository.getQuote()).thenReturn(expectedQuote)
 
+        Mockito.`when`(quoteRepository.getQuote()).thenReturn(expectedQuote)
         val mainViewModel = MainViewModel(quoteRepository)
         val actualQuote: PagingData<QuoteResponseItem> = mainViewModel.quote.getOrAwaitValue()
+
+        val differ = AsyncPagingDataDiffer(
+            diffCallback = QuoteListAdapter.DIFF_CALLBACK,
+            updateCallback = noopListUpdateCallback,
+            workerDispatcher = Dispatchers.Main,
+        )
+        differ.submitData(actualQuote)
     }
 }
+
+    val noopListUpdateCallback = object : ListUpdateCallback {
+        override fun onInserted(position: Int, count: Int) {}
+        override fun onRemoved(position: Int, count: Int) {}
+        override fun onMoved(fromPosition: Int, toPosition: Int) {}
+        override fun onChanged(position: Int, count: Int, payload: Any?) {}
+    }
 
 class QuotePagingSource : PagingSource<Int, LiveData<List<QuoteResponseItem>>>() {
     companion object {
