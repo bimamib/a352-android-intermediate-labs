6895421c7c5a61fa276e7f9fb3f5a3518dd44068
bimamib <bimaprast1601@gmail.com>
2023-10-23 23:00:45 +0700

    melanjutkan dengan membuat mekanisme mock pada saat memanggil data

--- advanced-database/LatihanPaging3 (starter)/app/src/test/java/com/dicoding/myunlimitedquotes/ui/MainViewModelTest.kt
+++ advanced-database/LatihanPaging3 (starter)/app/src/test/java/com/dicoding/myunlimitedquotes/ui/MainViewModelTest.kt
@@ -1,13 +1,24 @@
 package com.dicoding.myunlimitedquotes.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.dicoding.myunlimitedquotes.DataDummy
 import com.dicoding.myunlimitedquotes.MainDispatcherRule
 import com.dicoding.myunlimitedquotes.data.QuoteRepository
+import com.dicoding.myunlimitedquotes.getOrAwaitValue
+import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 @ExperimentalCoroutinesApi
@@ -21,4 +32,32 @@ class MainViewModelTest {
 
     @Mock
     private lateinit var quoteRepository: QuoteRepository
+
+    @Test
+    fun `when Get Quote Should Not Null and Return Data`() = runTest {
+        val dummyQuote = DataDummy.generateDummyQuoteResponse()
+        val data: PagingData<QuoteResponseItem> = QuotePagingSource.snapshot(dummyQuote)
+        val expectedQuote = MutableLiveData<PagingData<QuoteResponseItem>>()
+        expectedQuote.value = data
+        Mockito.`when`(quoteRepository.getQuote()).thenReturn(expectedQuote)
+
+        val mainViewModel = MainViewModel(quoteRepository)
+        val actualQuote: PagingData<QuoteResponseItem> = mainViewModel.quote.getOrAwaitValue()
+    }
+}
+
+class QuotePagingSource : PagingSource<Int, LiveData<List<QuoteResponseItem>>>() {
+    companion object {
+        fun snapshot(items: List<QuoteResponseItem>): PagingData<QuoteResponseItem> {
+            return PagingData.from(items)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, LiveData<List<QuoteResponseItem>>>): Int {
+        return 0
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LiveData<List<QuoteResponseItem>>> {
+        return LoadResult.Page(emptyList(), 0, 1)
+    }
 }
