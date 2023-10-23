05df1a8b3a969460689a5b11a6c99aca2a5ae468
bimamib <bimaprast1601@gmail.com>
2023-10-23 20:16:15 +0700

    menambahkan library, membuat kelas baru

---
+++ advanced-database/LatihanPaging3 (starter)/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuotePagingSource.kt
@@ -0,0 +1,16 @@
+package com.dicoding.myunlimitedquotes.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.dicoding.myunlimitedquotes.network.ApiService
+import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
+
+class QuotePagingSource(private val apiService: ApiService) : PagingSource<Int, QuoteResponseItem>() {
+    override fun getRefreshKey(state: PagingState<Int, QuoteResponseItem>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, QuoteResponseItem> {
+        TODO("Not yet implemented")
+    }
+}
