9b33e8aa74c8d5d30b1fa9488546433bba283f77
bimamib <bimaprast1601@gmail.com>
2023-10-23 15:46:02 +0700

    menambahkan parameter pada MainViewModel

--- advanced-database/LatihanRelasiRoom/MyStudentData (Starter)/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
+++ advanced-database/LatihanRelasiRoom/MyStudentData (Starter)/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
@@ -1,24 +1,38 @@
 package com.dicoding.mystudentdata
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.switchMap
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.database.StudentAndUniversity
 import com.dicoding.mystudentdata.database.StudentWithCourse
 import com.dicoding.mystudentdata.database.UniversityAndStudent
+import com.dicoding.mystudentdata.helper.SortType
 
-class MainViewModel(private val studentDao: StudentRepository) : ViewModel() {
+class MainViewModel(private val studentRepository: StudentRepository) : ViewModel() {
+
+    private val _sort = MutableLiveData<SortType>()
+    init {
+        _sort.value = SortType.ASCENDING
+    }
 
     // menghapus kode berikut
     /*init {
         insertAllData()
     }*/
 
-    fun getAllStudent(): LiveData<List<Student>> = studentDao.getAllStudent()
-    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
-    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUniversityAndStudent()
-    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentDao.getAllStudentWithCourse()
+    fun changeSortType(sortType: SortType) {
+        _sort.value = sortType
+    }
+
+    fun getAllStudent(): LiveData<List<Student>> = _sort.switchMap {
+        studentRepository.getAllStudent(it)
+    }
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentRepository.getAllStudentAndUniversity()
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentRepository.getAllUniversityAndStudent()
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentRepository.getAllStudentWithCourse()
 
     // menghapus kode berikut
     /*private fun insertAllData() = viewModelScope.launch {
